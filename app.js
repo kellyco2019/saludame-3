cd8b3a571fba00b20e498b94defcb5a420e42006
kellyco2019 <48420443+kellyco2019@users.noreply.github.com>
2020-09-24 13:51:23 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,17 +2,28 @@ const express = require('express');
 const app = express();
 
 function capitalize(string) {
-return string.charAt(0).toUpperCase() + string.slice(1);
-}
+  return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', function (req, res)  {
+  res.send(`<form action= '/' method = "post"> 
+  <input type ="text" name="name" /> 
+  <button type="submit">Enviar</button> 
+  </form>`);
+});
+app.post('/',function(req,res) {
+  const { nombre } = req.body;
+  
+  res.send(`<h1>Hola ${capitalize(nombre)}!</h1>`);
+});
 
 app.get('/makers/:nombre', (req, res) => {
     const { nombre } = req.params;
-      let template = " Hola ";
-
-      template += capitalize (nombre) + "!";
-
-      res.send(`<h1>${template}</h1>`);
+    
+    res.send(`<h1>Hola ${capitalize(nombre)}!</h1>`);
   });
-  
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
+  app.listen(3000, () => {
+   console.log('Listening on port 3000!');
+  });
